1d333f7e3bf6efcdcf0991e790cfd74d2622e66f
26-Anuj <59211195+Amitk6228@users.noreply.github.com>
2022-02-07 17:10:50 +0530

    Add project files.

---
+++ FirstMVCwebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using FirstMVCwebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstMVCwebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ FirstMVCwebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FirstMVCwebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
