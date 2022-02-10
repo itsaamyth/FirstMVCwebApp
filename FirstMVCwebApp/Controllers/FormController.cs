ffe435d073248748b23bf960124677a7e8a27bef
Amit Kumar <amit.kumar3@mail.vinove.com>
2022-02-10 19:14:56 +0530

    form page

---
+++ FirstMVCwebApp/Controllers/FormController.cs
@@ -0,0 +1,19 @@
+﻿using FirstMVCwebApp.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVCwebApp.Controllers
+{
+    public class FormController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public FormController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- FirstMVCwebApp/wwwroot/js/site.js
+++ FirstMVCwebApp/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function FillAddressInput() {
+    let checkBox = document.getElementById("checkBox");
+    let PerAddress = document.getElementById("PerAddress");
+    let curAddress = document.getElementById("CurAddress");
+    if (checkBox.checked == true) {
+
+        let PerAddressValue = PerAddress.value;
+        curAddress.value = PerAddressValue;
+        curAddress.disabled = true;
+    }
+    else {
+        curAddress.value = "";
+        curAddress.disabled = false;
+    }
+}
